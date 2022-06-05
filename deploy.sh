68b64d62fe1d941da833a407356320e3380184ba
Oleg Sudakov <oleg.sudakov@outlook.com>
2022-06-05 21:27:44 +0200

    Fix

--- deploy.sh
+++ deploy.sh
@@ -14,4 +14,4 @@ kubectl apply -f k8s
 
 kubectl set image deployments/client-deployment client=olegsudakov/multi-client:$SHA
 kubectl set image deployments/server-deployment server=olegsudakov/multi-server:$SHA
-kubectl set image deployments/worker-deployment client=olegsudakov/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=olegsudakov/multi-worker:$SHA
